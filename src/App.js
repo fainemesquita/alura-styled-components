87f9e44785a7f215f0bbcafb048353e4d63493c9
Faine Mesquita <faine.mesquita@leapdev.io>
2022-08-24 17:37:03 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ import { BtnTema } from "./Components/UI";
 import SwitcherTema from "./Components/SwitcherTema";
 
 function App() {
-  const [ tema, setTema ] = useState(true);
-  
+  const [tema, setTema] = useState(true);
+
   const toggleTema = () => {
     setTema((tema) => !tema);
   };
-  
+
   return (
     <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
       <GlobalStyle />
       <BtnTema onClick={toggleTema}>
-        <SwitcherTema thema={tema} />
+        <SwitcherTema tema={tema} />
       </BtnTema>
       <Cabecalho />
       <Container />
